af0f0bf66661ce36f5ccf61d04fcee18510a6674
ELODY FORT <elody.fort.professionnel@gmail.com>
2024-11-07 17:24:02 +0100

    charactercss

--- src/components/SelectCharacter.jsx
+++ src/components/SelectCharacter.jsx
@@ -1,50 +1,61 @@
 // SelectCharacter.jsx
 import React from "react";
+import "../css/SelectCharacter.css";
 
 function SelectCharacter() {
-  const characters = {
-    Elody: {
-      name: "Elody",
-      imgSrc: "src/assets/characters/Elody.svg"
-    },
-    Roxane: {
-      name: "Roxane",
-      imgSrc: "src/assets/characters/Roxane.svg"
-    },
-    Lucas: {
-      name: "Lucas",
-      imgSrc: "src/assets/characters/Lucas.svg"
-    },
-    Marjo: {
-      name: "Marjo",
-      imgSrc: "src/assets/characters/Marjo.svg"
-    },
-    Youssef: {
-      name: "Youssef",
-      imgSrc: "src/assets/characters/Youssef.svg"
-    },
-    Tom: {
-      name: "Tom",
-      imgSrc: "src/assets/characters/Tom.svg"
-    },
-    Yanis: {
-      name: "Yanis",
-      imgSrc: "src/assets/characters/Yanis.svg"
-    }
-  };
-  return (
-    <div>
-      <h2>Select Your Character</h2>
-      <div className="character-list">
-        {Object.values(characters).map((character, index) => (
-          <div key={index} className="character-item">
-            <img src={character.imgSrc} alt={character.name} />
-            <p>{character.name}</p>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+	const characters = {
+		Elody: {
+			name: "Elody",
+			imgSrc: "src/assets/characters/Elody.svg",
+		},
+		Roxane: {
+			name: "Roxane",
+			imgSrc: "src/assets/characters/Roxane.svg",
+		},
+		Lucas: {
+			name: "Lucas",
+			imgSrc: "src/assets/characters/Lucas.svg",
+		},
+		Marjo: {
+			name: "Marjo",
+			imgSrc: "src/assets/characters/Marjo.svg",
+		},
+		Youssef: {
+			name: "Youssef",
+			imgSrc: "src/assets/characters/Youssef.svg",
+		},
+		Tom: {
+			name: "Tom",
+			imgSrc: "src/assets/characters/Tom.svg",
+		},
+		Yanis: {
+			name: "Yanis",
+			imgSrc: "src/assets/characters/Yanis.svg",
+		},
+	};
+
+	// Composant pour afficher un personnage
+	const CharacterCard = ({ character }) => {
+		return (
+			<div className="character-container">
+				<div className="image-container">
+					<img
+						src={character.imgSrc} // Utilise imgSrc pour l'image
+						alt={character.name}
+						className="character-image"
+					/>
+				</div>
+			</div>
+		);
+	};
+
+	return (
+		<div className="character-list">
+			{Object.values(characters).map((character) => (
+				<CharacterCard key={character.name} character={character} />
+			))}
+		</div>
+	);
 }
 
 export default SelectCharacter;
