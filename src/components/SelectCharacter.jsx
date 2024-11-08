8e42f1aec40106adcda14c040d3e808633b345d1
xYanis <yanis.hortholary@gmail.com>
2024-11-08 09:54:06 +0100

    Ajout des boutons pour changer de personnage

--- src/components/SelectCharacter.jsx
+++ src/components/SelectCharacter.jsx
@@ -1,59 +1,54 @@
 // SelectCharacter.jsx
-import React from "react";
+import React, { useState } from "react";
 import "../css/SelectCharacter.css";
 
 function SelectCharacter() {
-	const characters = {
-		Elody: {
-			name: "Elody",
-			imgSrc: "src/assets/characters/Elody.svg",
-		},
-		Roxane: {
-			name: "Roxane",
-			imgSrc: "src/assets/characters/Roxane.svg",
-		},
-		Lucas: {
-			name: "Lucas",
-			imgSrc: "src/assets/characters/Lucas.svg",
-		},
-		Marjo: {
-			name: "Marjo",
-			imgSrc: "src/assets/characters/Marjo.svg",
-		},
-		Youssef: {
-			name: "Youssef",
-			imgSrc: "src/assets/characters/Youssef.svg",
-		},
-		Tom: {
-			name: "Tom",
-			imgSrc: "src/assets/characters/Tom.svg",
-		},
-		Yanis: {
-			name: "Yanis",
-			imgSrc: "src/assets/characters/Yanis.svg",
-		},
-	};
+	const characters = [
+		{ name: "Elody", imgSrc: "src/assets/characters/Elody.svg" },
+		{ name: "Roxane", imgSrc: "src/assets/characters/Roxane.svg" },
+		{ name: "Lucas", imgSrc: "src/assets/characters/Lucas.svg" },
+		{ name: "Marjo", imgSrc: "src/assets/characters/Marjo.svg" },
+		{ name: "Youssef", imgSrc: "src/assets/characters/Youssef.svg" },
+		{ name: "Tom", imgSrc: "src/assets/characters/Tom.svg" },
+		{ name: "Yanis", imgSrc: "src/assets/characters/Yanis.svg" },
+	];
 
-	// Composant pour afficher un personnage
-	const CharacterCard = ({ character }) => {
-		return (
-			<div className="character-container">
-				<div className="image-container">
-					<img
-						src={character.imgSrc} // Utilise imgSrc pour l'image
-						alt={character.name}
-						className="character-image"
-					/>
-				</div>
-			</div>
+	const [currentIndex, setCurrentIndex] = useState(0);
+
+	const showPreviousCharacter = () => {
+		setCurrentIndex((prevIndex) =>
+			prevIndex === 0 ? characters.length - 1 : prevIndex - 1
 		);
 	};
 
+	const showNextCharacter = () => {
+		setCurrentIndex((prevIndex) =>
+			prevIndex === characters.length - 1 ? 0 : prevIndex + 1
+		);
+	};
+
+	const CharacterCard = ({ character }) => (
+		<div className="character-container">
+			<div className="image-container">
+				<img
+					src={character.imgSrc}
+					alt={character.name}
+					className="character-image"
+				/>
+			</div>
+			<h2>{character.name}</h2>
+		</div>
+	);
+
 	return (
-		<div className="character-list">
-			{Object.values(characters).map((character) => (
-				<CharacterCard key={character.name} character={character} />
-			))}
+		<div className="character-selector">
+			<button onClick={showPreviousCharacter} className="arrow-button left">
+				{/* Bouton gauche avec triangle */}
+			</button>
+			<CharacterCard character={characters[currentIndex]} />
+			<button onClick={showNextCharacter} className="arrow-button right">
+				{/* Bouton droit avec triangle */}
+			</button>
 		</div>
 	);
 }
