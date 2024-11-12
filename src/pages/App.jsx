6dbf920ccc91687e2593edc7945ebc839b2e0a9b
ELODY FORT <elody.fort.professionnel@gmail.com>
2024-11-12 11:14:19 +0100

    fleches

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -3,12 +3,11 @@ import React from "react";
 import SelectCharacter from "../components/SelectCharacter";
 
 function App() {
-    return (
-      <div className="App">
-        <h1>Select Your Character</h1>
-        <SelectCharacter /> 
-      </div>
-    );
-  }
+	return (
+		<div className="App">
+			<SelectCharacter />
+		</div>
+	);
+}
 
 export default App;
